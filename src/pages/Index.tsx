c4af0723f986c872533b9b50261ee727aafe2366
SanjayPallapu <160562333+SanjayPallapu@users.noreply.github.com>
2026-01-05 15:45:38 +0530

    Shorten month names in Index component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -47,7 +47,10 @@ const Index = () => {
     role
   } = useAuth();
   const navigate = useNavigate();
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const months =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      
+      
+
   const handleSignOut = async () => {
     const {
       error
