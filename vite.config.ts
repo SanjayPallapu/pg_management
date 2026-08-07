c3898f9b4d9962c70f18bfbe5138a91331bc01cb
SanjayPallapu <sanjaykumar9390418552@gmail.com>
2026-08-07 08:47:16 +0530

    Update vite.config.tschore: redeploy trigger after making repo public

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite"; // redeploy trigger
+import { defineConfig } from "vite"; // redeploy trigger v2
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
